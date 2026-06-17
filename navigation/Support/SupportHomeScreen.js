0ab59b2d5be3fdb6e6cf369dfc5eed43b7d150ea
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:22:07 -0400

    Add SupportHomeScreen component

--- navigation/Support/SupportHomeScreen.js
+++ navigation/Support/SupportHomeScreen.js
@@ -1,1 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function SupportHomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Support</Text>
+    </View>
+  );
+}
 
