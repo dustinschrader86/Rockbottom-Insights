d8230b01f47b6c251f7da5a4b2f8668dbd1b2098
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-17 00:14:36 -0400

    Implement SearchNavigator with Search and Results screens

--- navigation/Search/SearchNavigator.js
+++ navigation/Search/SearchNavigator.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SearchScreen from './SearchScreen';
+import SearchResults from './SearchResults';
+
+const Stack = createNativeStackNavigator();
+
+export default function SearchNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="SearchScreen" 
+        component={SearchScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="SearchResults" 
+        component={SearchResults} 
+        options={{ title: 'Results' }}
+      />
+    </Stack.Navigator>
+  );
+}
 
