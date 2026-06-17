a368f04b2a3d33c08bf8a3e00a9ff25ce7971b7a
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:19:39 -0400

    Create EngineHomeScreen component

---
+++ navigation/Engine/EngineHomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function EngineHomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Engine</Text>
+    </View>
+  );
+}
