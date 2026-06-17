cd01cb3bac6cd8b23874e29b819ecdb02ba4556e
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 21:25:41 -0400

    Add Loader component with ActivityIndicator

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
 
+export const Loader = () => {
+  return (
+    <View
+      style={{
+        paddingVertical: 20,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ActivityIndicator size="large" color="#60A5FA" />
+    </View>
+  );
+};
