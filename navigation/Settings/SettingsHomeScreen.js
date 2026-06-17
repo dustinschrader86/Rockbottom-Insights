6ded9a99d3cbd747515247894946ecab7913697f
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:04:34 -0400

    Add SettingsHomeScreen component

--- navigation/Settings/SettingsHomeScreen.js
+++ navigation/Settings/SettingsHomeScreen.js
@@ -1,1 +1,9 @@
+import { View, Text } from 'react-native';
 
+export default function SettingsHomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
