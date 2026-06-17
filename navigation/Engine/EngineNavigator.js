53a8ebc61674a43bf765d30ddaade1dcd58972b3
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:18:55 -0400

    Add EngineNavigator with EngineHomeScreen

--- navigation/Engine/EngineNavigator.js
+++ navigation/Engine/EngineNavigator.js
@@ -1,1 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import EngineHomeScreen from './EngineHomeScreen';
+
+const Stack = createStackNavigator();
+
+export default function EngineNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="EngineHome"
+        component={EngineHomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 
