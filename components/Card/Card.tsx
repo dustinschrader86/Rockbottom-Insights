ed42c07970ae282d7d613f0f7f9401f63b6af15f
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 21:23:59 -0400

    Add Card component with customizable styles

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+import { View, ViewStyle } from "react-native";
+import { styles } from "./styles";
 
+type CardProps = {
+  children: React.ReactNode;
+  style?: ViewStyle | ViewStyle[];
+};
+
+export const Card: React.FC<CardProps> = ({ children, style }) => {
+  return <View style={[styles.card, style]}>{children}</View>;
+};
