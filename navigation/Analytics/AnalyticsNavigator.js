4e1c1e2c8ea27e27c03d73213f370ec29f88e01e
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:16:54 -0400

    Implement AnalyticsNavigator with stack navigation

--- navigation/Analytics/AnalyticsNavigator.js
+++ navigation/Analytics/AnalyticsNavigator.js
@@ -1,1 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import AnalyticsHomeScreen from './AnalyticsHomeScreen';
 
+const Stack = createStackNavigator();
+
+export default function AnalyticsNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="AnalyticsHome"
+        component={AnalyticsHomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
