430d75a2f9f5d9ed35e39e355156a157199d230d
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:04:01 -0400

    Add SettingsNavigator with SettingsHomeScreen

--- navigation/Settings/SettingsNavigator.js
+++ navigation/Settings/SettingsNavigator.js
@@ -1,1 +1,16 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import SettingsHomeScreen from './SettingsHomeScreen';
 
+const Stack = createStackNavigator();
+
+export default function SettingsNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="SettingsHome" 
+        component={SettingsHomeScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
