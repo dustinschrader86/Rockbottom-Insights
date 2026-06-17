0d85797b670c1cb945f9b9716e66954125013076
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:12:04 -0400

    Add ProfileHomeScreen component to HomeScreen.js

--- navigation/main-navigation/HomeScreen.js
+++ navigation/main-navigation/HomeScreen.js
@@ -1,1 +1,9 @@
+import { View, Text } from 'react-native';
 
+export default function ProfileHomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
