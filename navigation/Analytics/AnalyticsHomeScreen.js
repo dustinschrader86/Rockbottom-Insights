d1ca1c5004cb55f70e52997deffbd8fd1637468f
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-16 23:17:24 -0400

    Add AnalyticsHomeScreen component

--- navigation/Analytics/AnalyticsHomeScreen.js
+++ navigation/Analytics/AnalyticsHomeScreen.js
@@ -1,1 +1,9 @@
+import { View, Text } from 'react-native';
 
+export default function AnalyticsHomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Analytics</Text>
+    </View>
+  );
+}
